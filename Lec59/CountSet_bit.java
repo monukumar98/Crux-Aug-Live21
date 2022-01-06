8d2176297dcf56ebd03f18c51f51deccf534030f
monukumar98 <monu012as@gmail.com>
2022-01-06 11:09:23 +0530

    Bitmasking-Question

--- Lec59/CountSet_bit.java
+++ Lec59/CountSet_bit.java
@@ -4,7 +4,7 @@ public class CountSet_bit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int n = 89;
+		int n = 87;
 		System.out.println(CountSetBit(n));
 		System.out.println(FastCountSetBit(n));
 
